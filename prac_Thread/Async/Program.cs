ebbdd0ed711b7a45bb114992a5346e655f68b6c4
Seongho-Shin <ssh7636@gmail.com>
2021-12-16 16:15:47 +0900

    Async

---
+++ prac_Thread/Async/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class MainApp
+    {
+        async static private void MyMethodAsync(int count)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("D");
+
+            await Task.Run(async () =>
+            {
+                for (int i = 0; i <= count; i++)
+                {
+                    Console.WriteLine($"{i}/{count} ...");
+                    await Task.Delay(100);
+                }
+            });
+
+            Console.WriteLine("G");
+            Console.WriteLine("H");
+        }
+
+        static void Caller()
+        {
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+
+            MyMethodAsync(3);
+
+            Console.WriteLine("E");
+            Console.WriteLine("F");
+        }
+
+        static void Main(string[] args)
+        {
+            Caller();
+
+            Console.ReadLine();
+        }
+
+        /* Result
+            A
+            B
+            C
+            D
+            E
+            F
+            0/3 ...
+            1/3 ...
+            2/3 ...
+            3/3 ...
+            G
+            H
+        */
+    }
+}
